c6ff8a1af7d9518e087d4b348a837f2ec5f5fb8d
Qeti Khetsuriani <ketideveloper@gmail.com>
2024-10-15 21:38:56 +0400

    db

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+// import { connectToMongoDB } from "./common/db";
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const PORT: number = parseInt(process.env["PORT"] as string, 10);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-  
-});
+app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+async function main() {
+  // await connectToMongoDB();
+
+  app.use(cors());
+
+  app.get("/", (req: Request, res: Response) => {
+    res.send("Express + TypeScript Server");
+  });
+
+  app.listen(PORT, () => {
+    console.log(`[server]: Server is running at http://localhost:${PORT}`);
+  });
+}
+
+main();
